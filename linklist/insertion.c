70f10de578e8d11e437bfbf2011d7393d7509c08
Angela Maria Gigi <68962977+pappykunj@users.noreply.github.com>
2021-02-01 16:49:39 +0400

    link

--- linklist/insertion.c
+++ linklist/insertion.c
@@ -4,3 +4,21 @@ struct Node
     struct Node *link;
 }*header=NULL;
 
+void insertfront() 
+{
+    int ele;
+    struct Node*newnode;
+    printf("\nEnter the element to be inserted: ");
+    scanf("%d",&ele);
+    newnode=(struct Node*)malloc(sizeof(struct Node));
+    newnode->data=ele;
+    newnode->link=NULL;
+    if(header==NULL)
+        header=newnode;
+    else
+    {
+        newnode->link=header;
+        header=newnode;
+    }
+    printf("\nSuccessfully inserted");
+}
