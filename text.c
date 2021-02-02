51e7ea790028c2c856b8986adcecdcba9bdd8e7d
Angela Maria Gigi <68962977+pappykunj@users.noreply.github.com>
2021-02-02 22:47:19 +0400

    Update text.c

--- text.c
+++ text.c
@@ -1,5 +1,37 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <malloc.h>
+#include<string.h>
+
+struct person
+{
+    char name[50];
+    struct person *next;
+};
+
+struct ticket
+{
+    char number[50];
+    struct ticket *next;
+};
+
+struct person* create_person(char *);
+struct ticket* create_ticket(char *);
+
+void insert_person();
+void insert_ticket();
+void display_persons();
+void display_tickets();
+
+struct person *newperson;
+struct person *ptrPerson;
+struct ticket *newticket;
+struct ticket *ptrTicket;
+
+struct person * firstPerson = NULL;
+struct person * lastPerson = NULL;
+struct ticket * firstTicket = NULL;
+struct ticket * lastTicket = NULL; 
 
 struct book
 {
@@ -21,18 +53,16 @@ void book_ticket(); //for booking tickets
 
 void main()
 {
-
-	 int ch;
- 	do{
-	printf("\n\n\n");	
-	printf("\t ***MOVIE TICKETING SYSTEM***");
-	printf("\n\n");
-	
-	
-	printf("\n*Enter <1> Insert Movie");
-   	printf("\n*Enter <2> View All Movie");
-	printf("\n*Enter <3> Find Movie ");
-	printf("\n*Enter <4> Book Ticket");
+    int ch;
+    do
+    {
+ 	    printf("\n\n\n");	    
+ 	    printf("\t ***MOVIE TICKETING SYSTEM***");
+	    printf("\n\n");
+	    printf("\n*Enter <1> Insert Movie");
+   	    printf("\n*Enter <2> View All Movie");
+	    printf("\n*Enter <3> Find Movie ");
+	       printf("\n*Enter <4> Book Ticket");
    	printf("\n*Enter <0> Exit ");
 
    	printf("\nEnter your Choice ::");
@@ -49,9 +79,9 @@ void main()
 		case 3:
     			find();
    			break;
-		/*case 4:
+		case 4:
 			book_ticket();
-			break;	*/	
+			break;	
     		case 0:
     			exit(0);
     			break;
@@ -122,14 +152,15 @@ void find()
     else
     {
         temp=head;
-        while(temp->link!=NULL)
+        while(temp!=NULL)
         {
-            temp=temp->link;
+            
             if(temp->code==item)
             {
                 flag=1;
                 break;
             }
+            temp=temp->link;
         }
         if(flag==0)
             printf("The record not existing...\n");
@@ -146,4 +177,164 @@ void find()
     }
 }
 
+void book_ticket()
+{
+    int ch;
+    char ans = 'Y';
+
+    while (ans == 'Y'||ans == 'y')
+    {
+        printf("\n---------------------------------\n");
+        printf("\nOperations on Ticket & Queue\n");
+        printf("\n---------------------------------\n");
+        printf("\n1. Add Person in Q");
+        printf("\n2. Add ticket");
+        printf("\n3.Display All Persons in Q:");
+	    printf("\n4.Display All Tickets");
+        printf("\n10.Exit\n");
+        printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+        printf("\nEnter your choice : ");
+        scanf("%d", &ch);
+
+        switch (ch)
+        {
+            case 1: 
+            printf("\n...Adding Person in Q...\n");
+            insert_person();
+            break;
+
+            case 2: 
+            printf("\n...Adding Ticket...\n");
+            insert_ticket();
+            break;	
+            
+            case 3: 
+            printf("\n...Displaying Persons in Q From Beginning to End : \n");
+            display_persons();
+            break;
+            
+            case 4: 
+            printf("\n...Displaying Tickets From Beginning to End :\n");
+            display_tickets();
+            break;
+		
+            case 10: 
+            printf("\n...Exiting...\n");
+            break;
+            
+            default: 
+            printf("\n...Invalid Choice...\n");
+            break;
+        };
+        printf("\n\n You want to continue (Y/N)");
+        scanf(" %c", &ans);
+    }
+}
+ 
+struct person* create_person(char *ptrName)
+{
+    newperson = (struct person *)malloc(sizeof(struct person));
+    if (newperson == NULL)
+    {
+        printf("\nMemory was not allocated");
+        return 0;
+    }
+    else
+    {
+        strcpy(newperson->name, ptrName);
+        newperson->next = NULL;
+        return newperson;
+    }
+}
+ 
+struct ticket* create_ticket(char *tktNumber)
+{
+    newticket = (struct ticket *)malloc(sizeof(struct ticket));
+    if (newticket == NULL)
+    {
+        printf("\nMemory was not allocated");
+        return 0;
+    }
+    else
+    {
+        strcpy(newticket->number, tktNumber);
+        newticket->next = NULL;
+        return newticket;
+    }
+}
+
+ 
+void insert_person()
+{
+    char val[50];
+
+    printf("\nEnter the name of person : ");    
+    scanf("%s", val);
+    newperson = create_person(val);
+    if (firstPerson == lastPerson && lastPerson == NULL)
+    {
+        firstPerson = lastPerson = newperson;
+        firstPerson->next = NULL;
+        lastPerson->next = NULL;
+    }
+    else
+    {
+        lastPerson->next = newperson;
+        lastPerson = newperson;
+        lastPerson->next = NULL;
+    }
+    printf("\n----INSERTED----");
+}    
+
+void insert_ticket()
+{
+    char val[50];
+
+    printf("\nEnter the number of ticket : ");    
+    scanf("%s", val);
+    newticket = create_ticket(val);
+    if (firstTicket == lastTicket && lastTicket == NULL)
+    {
+        firstTicket = lastTicket = newticket;
+        firstTicket->next = NULL;
+        lastTicket->next = NULL;
+    }
+    else
+    {
+        lastTicket->next = newticket;
+        lastTicket = newticket;
+        lastTicket->next = NULL;
+    }
+    printf("\n----INSERTED----");
+} 
+
+void display_persons()
+{
+    if (firstPerson == NULL)
+    {
+        printf(":No Person in the list to display\n");
+    }
+    else
+    {
+        for (ptrPerson = firstPerson; ptrPerson != NULL;ptrPerson = ptrPerson->next)
+        {    
+            printf("%s\t", ptrPerson->name);
+        }
+    }
+}
+
+void display_tickets()
+{
+    if (firstTicket == NULL)
+    {
+        printf(":No Ticket in the list to display\n");
+    }
+    else
+    {
+        for (ptrTicket = firstTicket; ptrTicket != NULL;ptrTicket = ptrTicket->next)
+        {    
+            printf("%s\t", ptrTicket->number);
+        }
+    }
+} 
 
