8ba21cc81d0f6d12fef382bc2b5ac9c04fdd6c40
Angela Maria Gigi <angelamariagigi@gmail.com>
2023-05-24 09:05:53 +0530

    Update main.c

--- main.c
+++ main.c
@@ -33,7 +33,7 @@ void insert_ticket();		// to book tickets
 void display_persons();		// for entering peron
 void display_tickets();		// for entering ticket details
 void insert_details();		// for inserting movie details
-void view_all();		// for view the movies
+void view_all();		    // for view the movies
 void search_movie(); 		// for searching the moive
 void book_ticket(); 		// Menu to book tickets
 
